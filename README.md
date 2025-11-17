ba2272fa8c9aef0eec0fc5749e22d72dba4ed0ed
Forest McDonald <forest383@gmail.com>
2025-11-17 00:45:39 -0500

    A whole buncha junk, ready to create resources now

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # cloud-resume-2
 I am rebuilding my cloud resume from the ground up using Python 
+
+Need to set up the site as an Azure storage static website, then use a CDN for HTTPS. This might not work, as Azure has gone away with thier CDN Classic and now you have to use 
+Azure Front door, which is way more expensive.
+Looks like I should use Azure Static Web Apps as I can use Azure DNS to point to my custom domain: https://learn.microsoft.com/en-us/azure/static-web-apps/overview
+I can also still use GitHub Actions for CI/CD.
+I will use Lets encrypt for HTTPS on my domain: https://letsencrypt.org/getting-started/
+
+Creating the API that populates the visiter data on the site, use the MS tutorial, it creates the foundation for you: https://learn.microsoft.com/en-us/azure/azure-functions/how-to-create-function-vs-code?pivots=programming-language-python
+
+Ok I have the basic app created and tested that it works. I can supply a name via http request and it process it and returns a string.
+Now I need to modifiy the function and make it so it updates the data in Cosmos DB
+
+
+
+
+
+
+## List of azure services I need to create With Terraform
+Azure Static Web App CHECK
+Azure DNS? add this later when you have https figured out
+Cosmos DB account with a database and container CHECK
+Azure function app CHECK
+
+Terraform is ready for check and creation
+
+
+
+
---
+++ Terraform/main.tf
@@ -0,0 +1,100 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "azureResume2-rg" {
+  name     = "azureResume2.0-rg"
+  location = "East US"
+}
+
+resource "azurerm_static_web_app" "azureResume2-static-web-app" {
+  name                = "azureResume2-static-web-app"
+  resource_group_name = azurerm_resource_group.azureResume2-rg.name
+  location            = azurerm_resource_group.azureResume2-rg.location
+}
+
+# Storage Account for Function App
+resource "azurerm_storage_account" "azureResume2-storage-account" {
+  name                     = "azureResume2-storage-account"
+  resource_group_name      = azurerm_resource_group.azureResume2-rg.name
+  location                 = azurerm_resource_group.azureResume2-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# App Service Plan for Function App
+resource "azurerm_service_plan" "azureResume2-service-plan" {
+  name                = "azureResume2-service-plan"
+  resource_group_name = azurerm_resource_group.azureResume2-rg.name
+  location            = azurerm_resource_group.azureResume2-rg.location
+  os_type             = "Linux"
+  sku_name            = "Y1"  # Consumption plan
+}
+
+# Application Insights for monitoring
+resource "azurerm_application_insights" "azureResume2-app-insights" {
+  name                = "azureResume2-app-insights"
+  resource_group_name = azurerm_resource_group.azureResume2-rg.name
+  location            = azurerm_resource_group.azureResume2-rg.location
+  application_type    = "web"
+}
+
+# Linux Function App with Python runtime
+resource "azurerm_linux_function_app" "azureResume2-function-app" {
+  name                = "azureResume2-function-app"
+  resource_group_name = azurerm_resource_group.azureResume2-rg.name
+  location            = azurerm_resource_group.azureResume2-rg.location
+  service_plan_id     = azurerm_service_plan.azureResume2-service-plan.id
+  storage_account_name       = azurerm_storage_account.azureResume2-storage-account.name
+  storage_account_access_key = azurerm_storage_account.azureResume2-storage-account.primary_access_key
+
+  site_config {
+    application_stack {
+      python_version = "3.11"
+    }
+    use_32_bit_worker = false
+  }
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"       = "python"
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.azureResume2-app-insights.connection_string
+    "APPINSIGHTS_INSTRUMENTATIONKEY"        = azurerm_application_insights.azureResume2-app-insights.instrumentation_key
+  }
+}
+
+resource "azurerm_cosmosdb_account" "azureResume2-cosmosdb-account" {
+  name                  = "azureResume2-cosmosdb-account"
+  location              = azurerm_resource_group.azureResume2-rg.location
+  resource_group_name   = azurerm_resource_group.azureResume2-rg.name
+  offer_type            = "Standard"
+
+  consistency_policy {
+      consistency_level = "Strong"
+  }
+  
+  geo_location {
+      location          = azurerm_resource_group.azureResume2-rg.location
+      failover_priority = 0
+  }
+
+  capabilities {
+      name = "EnableServerless"
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "azureResume2-cosmosdb-database" {
+  name                = "azureResume2-cosmosdb-database"
+  resource_group_name = azurerm_cosmosdb_account.azureResume2-cosmosdb-account.resource_group_name
+  account_name        = azurerm_cosmosdb_account.azureResume2-cosmosdb-account.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "azureResume2-cosmosdb-container" {
+  name                  = "azureResume2-cosmosdb-container"
+  resource_group_name   = azurerm_cosmosdb_account.azureResume2-cosmosdb-account.resource_group_name
+  account_name          = azurerm_cosmosdb_account.azureResume2-cosmosdb-account.name
+  database_name         = azurerm_cosmosdb_sql_database.azureResume2-cosmosdb-database.name
+  partition_key_paths   = ["/counter/id"]
+  throughput            = 400
+
+}
+
